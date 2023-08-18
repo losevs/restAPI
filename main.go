4c1e888879bda0aaf3b7c2b70cffd79985f4d8e6
Sergey <hi.sergeyl@gmail.com>
2023-08-18 12:58:49 +0300

    add POST

--- main.go
+++ main.go
@@ -18,13 +18,23 @@ var todos = []todo{
 	{ID: "2", Name: "Cook dinner", Completed: false},
 }
 
-func sayHi(context *gin.Context) {
+func GetToDo(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, todos)
 	// context.Writer.Write([]byte("hello"))
 }
 
+func PostToDo(context *gin.Context) {
+	var newTodo todo
+	if err := context.BindJSON(&newTodo); err != nil {
+		return
+	}
+	todos = append(todos, newTodo)
+	context.IndentedJSON(http.StatusCreated, newTodo)
+}
+
 func main() {
 	router := gin.Default()
-	router.GET("/", sayHi)
+	router.GET("/", GetToDo)
+	router.POST("/new", PostToDo)
 	log.Fatalln(router.Run(":80"))
 }
